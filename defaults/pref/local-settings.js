de29227242954d3537d8773fa60ea1d28debffe9
BeatLink <beatlink@protonmail.com>
2019-08-16 19:49:17 -0500

    Initial Commit

---
+++ defaults/pref/local-settings.js
@@ -0,0 +1,7 @@
+// Sets up the .cfg file.
+
+//Specifies the .cfg filename (path cannot be changed)
+pref("general.config.filename", "librewolf.cfg"); 
+
+//Allows .cfg file to be stored in plain text without obfuscation
+pref("general.config.obscure_value", 0); 
