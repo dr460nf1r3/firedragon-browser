63b410f7ed34fbe7322558d5513477d133fb10dc
Nico Jensch <root@dr460nf1r3.org>
2023-12-27 12:37:21 +0100

    feat: use latest links for addons, update prefs for Floorp

--- defaults/pref/local-settings.js
+++ defaults/pref/local-settings.js
@@ -1,7 +1,6 @@
 // Sets up the .cfg file.
-
-//Specifies the .cfg filename (path cannot be changed)
+// Specifies the .cfg filename (path cannot be changed)
 pref("general.config.filename", "firedragon.cfg");
 
-//Allows .cfg file to be stored in plain text without obfuscation
+// Allows .cfg file to be stored in plain text without obfuscation
 pref("general.config.obscure_value", 0);
